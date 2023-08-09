5217adb47ce03488e8f788fe117ff2be609aeb80
SrujanRavuri <109953378+SrujanRavuri@users.noreply.github.com>
2023-08-09 10:37:25 +0530

    Update hotel.route.js

--- backend/routes/hotel.route.js
+++ backend/routes/hotel.route.js
@@ -14,4 +14,43 @@ router.route('/createhotel').post((req,res,next)=>{
         next(error);
     })
 })
+
+router.route('/').get((req,res)=>{
+    HotelSchema.find()
+    .then((hotel)=>{
+        res.json(hotel);
+    })
+    .catch((error)=>{
+        next(error);
+    })
+})
+
+router.route('/get-hotel/:id').get((req,res,next)=>{
+    HotelSchema.findById(req.params.id)
+    .then((hotel)=>{
+        res.json(hotel);
+    })
+    .catch((error)=>{
+        next(error);
+    })
+})
+
+router.route('/update-hotel/:id').put((req,res,next)=>{
+    HotelSchema.findByIdAndUpdate(
+        req.params.id,{
+            $set: req.body,
+        },
+    )
+    .then((hotel)=>{
+        res.json(hotel)
+        console.log("Hotel data updated!!!")
+    })
+    .catch((error)=>{
+        next(error)
+    })
+})
+
+router.route('/delete-hotel/:id').delete((req,res,next)=>{
+    
+})
 module.exports=router
