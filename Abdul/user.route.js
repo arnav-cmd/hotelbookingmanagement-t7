ce70634ff52ffb77fc6e1e5df552a7c58a772a9e
shaikabdulkalam <112329166+shaikabdulkalam@users.noreply.github.com>
2023-08-24 12:26:37 +0530

    Update user.route.js

--- Abdul/user.route.js
+++ Abdul/user.route.js
@@ -1,26 +1,19 @@
-const express = require("express");
-const router = express.Router();
-const jwt = require("jsonwebtoken");
-const User = require("../models/user"); 
-
 router.get("/MyProfilePage", async (req, res, next) => {
-  const token = req.headers.authorization.split(" ")[1]; 
-  try {
-    const decodedToken = jwt.verify(token, "12345"); 
-    const userId = decodedToken.userId;
-
-    const user = await User.findById(userId);
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
+    const token = req.headers.authorization.split(" ")[1];
+    try {
+      const decodedToken = jwt.verify(token, "12345"); // Replace with your actual secret key
+      const userId = decodedToken.userId;
+  
+      const user = await userSchema.findById(userId);
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+      res.json({
+        Username: user.Username,
+        email: user.email,
+        mobile: user.mobile,
+      });
+    } catch (error) {
+      res.status(401).json(error.message);
     }
-    res.json({
-      Username: user.Username,
-      email: user.email,
-      mobile: user.mobile,
-    });
-  } catch (error) {
-    res.status(401).json({ message: "Unauthorized" });
-  }
-});
-
-module.exports = router;
+  });
