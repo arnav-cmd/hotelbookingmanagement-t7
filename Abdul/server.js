e8dfccbdb9d2c99a33a680658d185dd2e0b4ccc3
shaikabdulkalam <112329166+shaikabdulkalam@users.noreply.github.com>
2023-08-24 12:27:12 +0530

    Update server.js

--- Abdul/server.js
+++ Abdul/server.js
@@ -1,48 +1,188 @@
 let express = require("express");
+
 let mongoose = require("mongoose");
+
 let cors = require("cors");
+
+let bcrypt=require("bcrypt");
+
 let bodyParser = require("body-parser");
 
-const HotelRoute=require('./routes/hotel.route');
-const AdminloginRoute=require('./routes/adminlogin.route');
-const UserloginRoute=require('./routes/userlogin.route');
+let jwt=require("jsonwebtoken");
 
-const Hotel=require('./models/hotels');
-const adminlogin=require('./models/adminlogin');
-const userlogin=require('./models/user');
+const usermyprofile = require("../backend/routes/user.route")
+ 
+
+const secretkey="12345";
+
+const HotelRoute = require("./routes/hotel.route");
+
+const userRoute = require("./routes/user.route");
+
+// const adminRoute=require("./routes/admin.route");
+
+const User = require("./models/user");
+
+const Hotel = require("./models/hotels");
+
+// const Admin=require("./models/admin");
 
 mongoose
-.connect('mongodb://127.0.0.1:27017/HotelsInfo')
-.then((x)=>{
-    console.log(`Connected to MongoDb! Database Name:"${x.connections[0].name}"`);
-})
-.catch((error)=>{
-    console.log("Error connecting to mongo",error.reason)
-});
-const app=express();
+
+  .connect("mongodb://127.0.0.1:27017/HotelsInfo")
+
+  .then((x) => {
+
+    console.log("Connected to MongoDb!");
+
+  })
+
+  .catch((error) => {
+
+    console.log("Error connecting to mongo", error.reason);
+
+  });
+
+ 
+
+app = express();
+
 app.use(bodyParser.json());
+
 app.use(
-    bodyParser.urlencoded({
-        extended:true,
-    })
+
+  bodyParser.urlencoded({
+
+    extended: true,
+
+  })
+
 );
-app.use(cors())
 
-app.use("/Hotels",HotelRoute);
-app.use("/adminlogins",AdminloginRoute);
-app.use("/users",UserloginRoute);
+app.use(cors());
 
-const port=process.env.PORT || 4000;
-const server=app.listen(port,()=>{
-    console.log("Connected to port",port);
+app.use("/Hotels", HotelRoute);
+
+app.use("/users", userRoute);
+
+app.use("/user",usermyprofile);
+
+const port = process.env.PORT || 4000;
+
+const server = app.listen(port, () => {
+
+  console.log("Connected to port", port);
+
 });
 
+ 
+
+app.post("/login", async (req, res) => {
+
+  const { email, password} = req.body;
+
+  console.log(email);
+
+  try {
+
+    const user = await User.findOne({ email: email });
+
+    if (!user) {
+
+      console.log("User not found:", email);
+
+      return res.json("notexist");
+
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    console.log("Password Valid:", isPasswordValid);
+
+    if (isPasswordValid) {
+
+     
+
+      console.log(user.isAdmin);
+
+      const token=jwt.sign({userId:user._id,isAdmin:user.isAdmin},secretkey,{expiresIn:'1h',})
+
+      res.json({token,user:{email:user.email,isAdmin:user.isAdmin}});
+
+      console.log(token);
+
+    } else {
+
+      res.json({token:null});
+
+    }
+
+  } catch (e) {
+
+    console.error("Login error:", e.message);
+
+    res.json("fail");
+
+  }
+
+});
+
+// app.post("/adminlogin", async (req, res) => {
+
+//   const { email, password } = req.body;
+
+//   console.log(email);
+
+//   try {
+
+//     const user = await Admin.findOne({ email: email });
+
+//     if (!user) {
+
+//       console.log("Admin not found:", email);
+
+//       return res.json("notexist");
+
+//     }
+
+//     const isPasswordValid = await bcrypt.compare(password, user.password);
+
+//     console.log("Password Valid:", isPasswordValid);
+
+//     if (isPasswordValid) {
+
+//       res.json("success");
+
+//     } else {
+
+//       res.json("invalid");
+
+//     }
+
+//   } catch (e) {
+
+//     console.error("Login error:", e.message);
+
+//     res.json("fail");
+
+//   }
+
+// });
 
 app.use((req, res, next) => {
-    next(createError(404));
-  });
-  app.use(function (err, req, res, next) {
-    console.error(err.message);
-    if (!err.statusCode) err.statusCode = 500;
-    res.status(err.statusCode).send(err.message);
-  });
+
+  next(createError(404));
+
+});
+
+ 
+
+app.use(function (err, req, res, next) {
+
+  console.error(err.message);
+
+  if (!err.statusCode) err.statusCode = 500;
+
+  res.status(err.statusCode).send(err.message);
+
+});
